85335fecba77332531bcc2cf1974fd4c169573c4
Akashi359 <46638734+Akashi359@users.noreply.github.com>
2021-03-27 17:23:31 -0700

    Delete Message.h
    Manually removed file that wouldn't seem to disappear.

--- WindowsProject2/Message.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#pragma once
-#include <stdint.h>
-
-size_t getMessage(const char* dest, const char* command, int commandLen, uint8_t*&);
-
-char* getAddress(const char* msg);
-
-int count(const char*, char, unsigned);
